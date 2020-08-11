7704f25875458408935fb60ef992d1c75fe83e32
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-11 14:46:23 +0300

    Tietomeri WordPress plugin

---
+++ Ukuli-Tietomeri-WordPress-Plugin.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Plugin Name:       Ukuli Tietomeri WordPress Plugin
+ * Plugin URI:        https://tietomeri.digital
+ * Description:       WordPress plugin for Tietomeri.
+ * Version:           1.0
+ * Author:            Tomi Toivio
+ * Author URI:        https://ukuli.fi/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       ukuli
+ * Domain Path:       /languages
+ */
